8fd3be298907818c8dc0703bf33b3adde314793a
Esra Al <esra.wameed@yahoo.com>
2022-09-16 15:48:26 -0400

    added multiplt if/else statements depending on userInput of what character options they want their pass to have, asked for confirmation, and alerted them of what will be included depending on their selection

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,15 +15,40 @@ function writePassword() {
         let passCharacters = []
 // function to generate password
 function generatePassword() {
-    let passLength = parseInt(prompt("How many characters would you like your password to be (must be from 8-128)"));
+        let passLength = parseInt(prompt("How many characters would you like your password to be (must be from 8-128)"));
 //range for acceptable user prompt input
-    if (passLength > 128 || passLength < 8 || isNaN(passLength) ) { 
-        alert("Please ensure value entered is a number and the number is from 8-128");
-        return generatePassword();
-    } else{
+             if (passLength > 128 || passLength < 8 || isNaN(passLength) ) { 
+                alert("Please ensure value entered is a number and the number is from 8-128");
+                return generatePassword();
+            } else{
         window.alert("Your password will be " + passLength+ " characters long" );
-    }
-
+         }
+//alerts displayed depending on user choice
+        let userInput = [];
+        let UpperCasing = confirm("Includes uppercase letters: press OK for yes and CANCEL for no");
+            if(UpperCasing){
+                 window.alert("your password will include an uppercase letter");
+            }else{
+            window.alert("your password will NOT contain an uppercase letter");
+        }
+        let lowerCasing = confirm("Includes lowerrcase letters: press OK for yes and CANCEL for no");
+            if(lowerCasing){
+            window.alert("your password will contain a lower case letter");
+            }else{
+            window.alert("your password will NOT contain a lower case letter");
+        }
+        let passNumber = confirm("Includes numbers: press OK for yes and CANCEL for no");
+            if(passNumber){
+            window.alert( "your password will contain a number");
+            }else{
+            window.alert("your password will NOT contain a number");
+        }
+        let charSymbol = confirm("Includes symbols: press OK for yes and CANCEL for no");
+            if(charSymbol){
+            window.alert("your password will contain a symbol");
+            }else{
+            window.alert("your password will NOT contain a symbol");
+        }
 
 
 
