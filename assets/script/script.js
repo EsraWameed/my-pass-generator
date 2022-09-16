6fd21704b4f3d850285ccd52e4f8b06b3290abce
Esra Al <esra.wameed@yahoo.com>
2022-09-16 15:40:30 -0400

    changed var values to let and created variables representing the 4 character criteria options

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,26 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+        let password = generatePassword();
+        let passwordText = document.querySelector("#password");
+            passwordText.value = password;
+        }
+//Character options displayed for user displayed and will be pushed to array passCharacters
+        let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        let specialCharacters = ["[\]","^", ".","!", "@", "#", "$", "%", "{","}", "&", "*", "(", ")", "+", ",", "-", "/", ";", "?", "]", "|", "`"];
+        let passCharacters = []
 
-  passwordText.value = password;
 
-}
+
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
